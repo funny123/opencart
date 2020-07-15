0076da4f2bec42982945a45108ae3d487a1a7548
funny123 <524656859@qq.com>
2020-07-15 17:59:58 +0800

    3.0.3.2

---
+++ upload/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://opencart.test/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://opencart.test/');
+
+// DIR
+define('DIR_APPLICATION', '/www/wwwroot/opencart.test/upload/catalog/');
+define('DIR_SYSTEM', '/www/wwwroot/opencart.test/upload/system/');
+define('DIR_IMAGE', '/www/wwwroot/opencart.test/upload/image/');
+define('DIR_STORAGE', '/www/wwwroot/opencart.test/upload/storage/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/theme/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mpdo');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'opencart_test');
+define('DB_PASSWORD', '111111');
+define('DB_DATABASE', 'opencart_test');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
